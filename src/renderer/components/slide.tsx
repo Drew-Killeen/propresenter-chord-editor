5105acd50f6f8b707000df7088518c59fda34bed
Drew Killeen <drewkilleen00@gmail.com>
2023-10-05 20:14:59 -0400

    Make slide editable

--- src/renderer/components/slide.tsx
+++ src/renderer/components/slide.tsx
@@ -14,7 +14,9 @@ export default function Slide({
         <div className="slide-header-label">{label}</div>
         <div className="empty-spacer" />
       </div>
-      <div className="slide-body">{children}</div>
+      <div className="slide-body" contentEditable>
+        {children}
+      </div>
     </div>
   );
 }
